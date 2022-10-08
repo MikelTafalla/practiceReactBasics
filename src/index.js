7c6a1c544488686b35acf736a338ce034d4143ce
Mikel <mikel362d@gmail.com>
2022-10-08 11:23:25 -0700

    adds approval card styling as a new component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import { faker } from "@faker-js/faker";
 
 const App = () => {
     return (
         <div className = "ui container comments">
-            <CommentDetail 
-                author = "Sam"
-                date = "Today at 4:45 PM"
-                comment = "Nice Blog!"
-                image = {faker.image.image()}
-            />
-            <CommentDetail
-                author = "Alex"
-                date = "Today at 2:00 PM"
-                comment = "Hate the food here!"
-                image = {faker.image.image()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Sam"
+                    date = "Today at 4:45 PM"
+                    comment = "Nice Blog!"
+                    image = {faker.image.image()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author = "Alex"
+                    date = "Today at 2:00 PM"
+                    comment = "Hate the food here!"
+                    image = {faker.image.image()}
+                />
+            </ApprovalCard>
         </div>
     );
 }
